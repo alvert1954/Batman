0af4ba538a202608eb1046ccc14dc165c9ca5ffb
alvert1954 <35923023+alvert1954@users.noreply.github.com>
2022-02-15 00:35:51 +0300

    Сверстал мобильную версию

--- js/main.js
+++ js/main.js
@@ -5,4 +5,24 @@ const swiper = new Swiper('.main-slider', {
   navigation: {
     nextEl: '.main-slider__arrow',
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+let burger = document.querySelector('.menu-burger');
+let closeMenu = document.querySelector('.header__menu-close');
+let menuMobile = document.querySelector('.header');
+
+burger.addEventListener('click', () => {
+  menuMobile.style.display = 'block';
+});
+
+closeMenu.addEventListener('click', () => {
+  menuMobile.style.display = 'none';
 });
